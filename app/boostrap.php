9923a2947ab3e15579182c1d8099dea4c84dcd72
JirikPos <Jirikpos221@gmail.com>
2025-05-02 00:13:59 +0200

    created esp branch

--- app/boostrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_start();
-header('Content-Type: text/html; charset=utf-8');
-
-$config = require_once __DIR__ . '/../.env.php';
-require_once __DIR__ . '/Utils/Database.php';
-require_once __DIR__ . '/Utils/Logger.php';
