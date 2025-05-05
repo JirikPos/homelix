660f4edc6f3e3e54326ec0ccbb34d2da463ac5b9
skrivanj <skrivan.j07@gmail.com>
2025-05-05 23:34:38 +0200

    pridání meta elementů

--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Jiří Posavád, Jan Skřivan, Tomáš Vozka">
+    <meta name="keywords" content="smart home, chytrá domácnost, domácí automatizace, inteligentní dům, smart vybavení">
+    <meta name="description" content="Vylepšete svůj domov pomocí chytré domácnosti. Ovládejte světla, topení a bezpečnostní prvky odkudkoli – jednoduše, úsporně a bezpečně.">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Homelix</title>
 </head>
 
@@ -21,7 +25,7 @@
     </main>
 
     <footer>
-        
+
     </footer>
 </body>
 
