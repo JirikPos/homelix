9da854dbea16e0887f3f85110384f04ad68f0335
JirikPos <Jirikpos221@gmail.com>
2025-05-12 17:00:21 +0200

    created design

--- public/pages/dashboard.php
+++ public/pages/dashboard.php
@@ -1,1 +1,269 @@
+<!DOCTYPE html>
+<html>
 
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="initial-scale=1, width=device-width">
+  <link rel="stylesheet" href="../css/style.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Apple LiGothic:wght@500&display=swap" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;500;600&display=swap" />
+</head>
+
+<body>
+  <div class="homebedroom-dashboard">
+    <div class="frame-parent">
+      <div class="homelix-parent">
+        <div class="homelix">Homelix</div>
+      </div>
+      <div class="tab-controller">
+        <div class="o-projektu-parent">
+          <div class="o-projektu">O projektu</div>
+          <img class="fi-10206874-icon" alt="" src="../assets/icons/about.svg">
+
+        </div>
+        <div class="tab-controller-child">
+        </div>
+        <div class="ovldac-panel-parent">
+          <div class="ovldac-panel">Ovládací panel</div>
+          <img class="home2alt-icon" alt="" src="../assets/icons/house.svg">
+        </div>
+      </div>
+      <div class="navnotifications-parent">
+        <img class="navprofile-icon" alt="" src="../assets/icons/settings.svg">
+      </div>
+    </div>
+    <div class="frame-group">
+      <div class="ac-widget-parent">
+        <div class="ac-widget">
+          <div class="frame-container">
+            <div class="frame-parent5">
+              <img class="frame-child" alt="" src="../assets/icons/wind.svg">
+              <div class="index-kvality-ovzdu-parent">
+                <div class="index-kvality-ovzdu">Index kvality ovzduší</div>
+                <div class="wrapper">
+                  <div class="ovldac-panel">1500</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="toggle-parent">
+            <div class="toggle">
+              <div class="track">
+              </div>
+              <div class="knob">
+              </div>
+            </div>
+            <div class="medium">Kvalita ovzduší</div>
+          </div>
+        </div>
+        <div class="ac-widget">
+          <div class="frame-container">
+            <div class="frame-parent5">
+              <div class="upvariant3-parent">
+                <img class="upvariant3-icon" alt="" src="Up/Variant3.svg">
+
+                <div class="up">
+                  <img class="frame-icon" alt="" src="Frame.svg">
+
+                </div>
+              </div>
+              <div class="index-kvality-ovzdu-parent">
+                <div class="index-kvality-ovzdu">Aktuální vlhkost</div>
+                <div class="parent">
+                  <div class="ovldac-panel">%</div>
+                  <div class="ovldac-panel">50</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="frame-container">
+            <div class="frame-parent5">
+              <div class="upvariant3-parent">
+                <img class="upvariant3-icon" alt="" src="Up/Variant3.svg">
+                <div class="up">
+                  <img class="frame-icon" alt="" src="">
+                </div>
+              </div>
+              <div class="index-kvality-ovzdu-parent">
+                <div class="index-kvality-ovzdu">Aktuální teplota</div>
+                <div class="parent">
+                  <div class="ovldac-panel">
+                    <span>°</span>
+                    <span class="c1"> C</span>
+                  </div>
+                  <div class="ovldac-panel">18</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="toggle-parent">
+            <div class="toggle">
+              <div class="track">
+              </div>
+              <div class="knob">
+              </div>
+            </div>
+            <div class="medium">Teplota a vhlkost</div>
+          </div>
+        </div>
+      </div>
+      <div class="menu-options-parent">
+        <div class="menu-options">
+          <img class="menu-options-child" alt="" src="Frame 24.svg">
+
+          <div class="frame-parent">
+            <div class="toggle">
+              <div class="track">
+              </div>
+              <div class="knob">
+              </div>
+            </div>
+            <div class="zvukov-vstraha">Zvuková výstraha</div>
+          </div>
+        </div>
+        <div class="menu-options">
+          <img class="menu-options-child" alt="" src="Frame 24.svg">
+
+          <div class="frame-parent">
+            <div class="toggle">
+              <div class="track">
+              </div>
+              <div class="knob">
+              </div>
+            </div>
+            <div class="zvukov-vstraha">Pohybové čidlo PIR</div>
+          </div>
+        </div>
+        <div class="menu-options2">
+          <div class="menu-options-inner">
+            <div class="frame-parent20">
+              <div class="alert-sign-parent">
+                <img class="alert-sign-icon" alt="" src="../assets/icons/alert-sign.svg">
+                <img class="home2alt-icon" alt="" src="../assets/icons">
+              </div>
+              <div class="index-kvality-ovzdu-group">
+                <div class="index-kvality-ovzdu">Index kvality ovzduší</div>
+                <div class="zjitn-nik-vody-wrapper">
+                  <div class="ovldac-panel">Zjištěn únik vody</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="nik-vody">Únik vody</div>
+        </div>
+      </div>
+      <div class="menu-options-group">
+        <div class="menu-options3">
+          <div class="frame-parent">
+            <div class="toggle">
+              <div class="track4">
+              </div>
+              <div class="knob4">
+              </div>
+            </div>
+            <div class="zvukov-vstraha">Bezpečný režim</div>
+          </div>
+        </div>
+        <div class="menu-options3">
+          <div class="frame-parent">
+            <div class="toggle">
+              <div class="track4">
+              </div>
+              <div class="knob4">
+              </div>
+            </div>
+            <div class="zvukov-vstraha">Hlídání úniku vody</div>
+          </div>
+        </div>
+        <div class="menu-options3">
+          <div class="frame-parent">
+            <div class="toggle">
+              <div class="track4">
+              </div>
+              <div class="knob4">
+              </div>
+            </div>
+            <div class="zvukov-vstraha">Noční režim</div>
+          </div>
+        </div>
+        <div class="menu-options">
+          <div class="menu-options-inner1">
+            <div class="menu-options-container">
+              <img class="menu-options-icon" alt="" src="Menu options.png">
+              <img class="menu-options-icon" alt="" src="Menu options.png">
+              <img class="menu-options-icon" alt="" src="Menu options.png">
+              <img class="menu-options-icon" alt="" src="Menu options.png">
+              <img class="menu-options-icon" alt="" src="Menu options.png">
+            </div>
+          </div>
+          <div class="frame-parent">
+            <div class="toggle">
+              <div class="track">
+              </div>
+              <div class="knob">
+              </div>
+            </div>
+            <div class="zvukov-vstraha">Scény</div>
+          </div>
+        </div>
+      </div>
+      <div class="menu-options7">
+        <div class="frame-parent21">
+          <div class="menu-options-inner">
+            <div class="frame-parent22">
+              <div class="frame-parent">
+                <div class="off-wrapper">
+                  <div class="medium">Off</div>
+                </div>
+                <div class="wind">Music Sync</div>
+              </div>
+              <div class="frame-parent">
+                <div class="off-wrapper">
+                  <div class="medium">Setup</div>
+                </div>
+                <div class="wind">Schedule</div>
+              </div>
+              <div class="frame-parent">
+                <div class="off-wrapper">
+                  <div class="medium">Gorgeous</div>
+                </div>
+                <div class="wind">Scene</div>
+              </div>
+              <div class="frame-parent">
+                <div class="off-wrapper">
+                  <div class="medium">White</div>
+                </div>
+                <div class="wind">Colour</div>
+              </div>
+            </div>
+          </div>
+          <div class="frame-parent27">
+            <div class="frame-parent28">
+              <img class="fi-10206874-icon" alt="" src="Frame.svg">
+
+              <div class="zvukov-vstraha">27%</div>
+            </div>
+            <img class="frame-item" alt="" src="Group 13.png">
+
+            <div class="wind">Brightness</div>
+          </div>
+        </div>
+        <div class="toggle-parent">
+          <div class="toggle">
+            <div class="track4">
+            </div>
+            <div class="knob4">
+            </div>
+          </div>
+          <div class="medium">Ambient lights</div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+</body>
+
+</html>
