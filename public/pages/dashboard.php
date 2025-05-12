0e275cb467ac971efa82bed90be45d858e55f193
skrivanj <skrivan.j07@gmail.com>
2025-05-12 23:48:00 +0200

    dashboard-zmeny

--- public/pages/dashboard.php
+++ public/pages/dashboard.php
@@ -10,258 +10,151 @@
 </head>
 
 <body>
-  <div class="homebedroom-dashboard">
-    <div class="frame-parent">
-      <div class="homelix-parent">
-        <div class="homelix">Homelix</div>
+  <div class="frame-parent">
+    <div class="homelix-parent">
+      <div class="homelix">Homelix</div>
+      <img class="navmenu-icon" alt="" src="Nav/Menu.svg">
+    </div>
+    <div class="tab-controller">
+      <div class="o-projektu-parent">
+        <div class="o-projektu">O projektu</div>
+        <img class="fi-10206874-icon" alt="" src="fi_10206874.svg">
       </div>
-      <div class="tab-controller">
-        <div class="o-projektu-parent">
-          <div class="o-projektu">O projektu</div>
-          <img class="fi-10206874-icon" alt="" src="../assets/icons/about.svg">
-
-        </div>
-        <div class="tab-controller-child">
-        </div>
-        <div class="ovldac-panel-parent">
-          <div class="ovldac-panel">Ovládací panel</div>
-          <img class="home2alt-icon" alt="" src="../assets/icons/house.svg">
-        </div>
+      <div class="tab-controller-child">
       </div>
-      <div class="navnotifications-parent">
-        <img class="navprofile-icon" alt="" src="../assets/icons/settings.svg">
+      <div class="ovldac-panel-parent">
+        <div class="ovldac-panel">Ovládací panel</div>
+        <img class="home2alt-icon" alt="" src="home/2/alt.svg">
+        <img class="fi-1047105-icon" alt="" src="fi_1047105.svg">
       </div>
     </div>
-    <div class="frame-group">
-      <div class="ac-widget-parent">
-        <div class="ac-widget">
-          <div class="frame-container">
-            <div class="frame-parent5">
-              <img class="frame-child" alt="" src="../assets/icons/wind.svg">
-              <div class="index-kvality-ovzdu-parent">
-                <div class="index-kvality-ovzdu">Index kvality ovzduší</div>
-                <div class="wrapper">
-                  <div class="ovldac-panel">1500</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="toggle-parent">
-            <div class="toggle">
-              <div class="track">
-              </div>
-              <div class="knob">
-              </div>
-            </div>
-            <div class="medium">Kvalita ovzduší</div>
-          </div>
-        </div>
-        <div class="ac-widget">
-          <div class="frame-container">
-            <div class="frame-parent5">
-              <div class="upvariant3-parent">
-                <img class="upvariant3-icon" alt="" src="Up/Variant3.svg">
-
-                <div class="up">
-                  <img class="frame-icon" alt="" src="Frame.svg">
-
-                </div>
-              </div>
-              <div class="index-kvality-ovzdu-parent">
-                <div class="index-kvality-ovzdu">Aktuální vlhkost</div>
-                <div class="parent">
-                  <div class="ovldac-panel">%</div>
-                  <div class="ovldac-panel">50</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="frame-container">
-            <div class="frame-parent5">
-              <div class="upvariant3-parent">
-                <img class="upvariant3-icon" alt="" src="Up/Variant3.svg">
-                <div class="up">
-                  <img class="frame-icon" alt="" src="">
-                </div>
-              </div>
-              <div class="index-kvality-ovzdu-parent">
-                <div class="index-kvality-ovzdu">Aktuální teplota</div>
-                <div class="parent">
-                  <div class="ovldac-panel">
-                    <span>°</span>
-                    <span class="c1"> C</span>
-                  </div>
-                  <div class="ovldac-panel">18</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="toggle-parent">
-            <div class="toggle">
-              <div class="track">
-              </div>
-              <div class="knob">
-              </div>
-            </div>
-            <div class="medium">Teplota a vhlkost</div>
+    <div class="navnotifications-parent">
+      <div class="navnotifications">
+        <div class="alert-vector-parent">
+          <img class="alert-vector-icon" alt="" src="Alert vector.svg">
+          <div class="nudge">
           </div>
         </div>
       </div>
-      <div class="menu-options-parent">
-        <div class="menu-options">
-          <img class="menu-options-child" alt="" src="Frame 24.svg">
+      <img class="navprofile-icon" alt="" src="Nav/Profile.png">
+    </div>
+  </div>
 
-          <div class="frame-parent">
-            <div class="toggle">
-              <div class="track">
-              </div>
-              <div class="knob">
-              </div>
-            </div>
-            <div class="zvukov-vstraha">Zvuková výstraha</div>
-          </div>
+  <div class="dashboard-columns">
+    <div class="column">
+      <div class="card">
+        <div class="card-header">
+          <span>Kvalita ovzduší</span>
+          <label class="switch">
+            <input type="checkbox">
+            <span class="slider"></span>
+          </label>
         </div>
-        <div class="menu-options">
-          <img class="menu-options-child" alt="" src="Frame 24.svg">
-
-          <div class="frame-parent">
-            <div class="toggle">
-              <div class="track">
-              </div>
-              <div class="knob">
-              </div>
-            </div>
-            <div class="zvukov-vstraha">Pohybové čidlo PIR</div>
-          </div>
-        </div>
-        <div class="menu-options2">
-          <div class="menu-options-inner">
-            <div class="frame-parent20">
-              <div class="alert-sign-parent">
-                <img class="alert-sign-icon" alt="" src="../assets/icons/alert-sign.svg">
-                <img class="home2alt-icon" alt="" src="../assets/icons">
-              </div>
-              <div class="index-kvality-ovzdu-group">
-                <div class="index-kvality-ovzdu">Index kvality ovzduší</div>
-                <div class="zjitn-nik-vody-wrapper">
-                  <div class="ovldac-panel">Zjištěn únik vody</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="nik-vody">Únik vody</div>
+        <div class="card-value red">1500</div>
+        <div class="card-footer">
+          <span class="description">Index kvality ovzduší</span>
+          <img src="alert-icon.svg" alt="Varování" class="alert-icon">
         </div>
       </div>
-      <div class="menu-options-group">
-        <div class="menu-options3">
-          <div class="frame-parent">
-            <div class="toggle">
-              <div class="track4">
-              </div>
-              <div class="knob4">
-              </div>
-            </div>
-            <div class="zvukov-vstraha">Bezpečný režim</div>
-          </div>
+
+      <div class="card">
+        <div class="card-header">
+          <span>Teplota a vlhkost</span>
+          <label class="switch"><input type="checkbox"><span class="slider"></span></label>
         </div>
-        <div class="menu-options3">
-          <div class="frame-parent">
-            <div class="toggle">
-              <div class="track4">
-              </div>
-              <div class="knob4">
-              </div>
-            </div>
-            <div class="zvukov-vstraha">Hlídání úniku vody</div>
-          </div>
+        <div class="card-value">25 °C</div>
+        <div class="card-footer">
+          <span class="description">Aktuální teplota</span>
         </div>
-        <div class="menu-options3">
-          <div class="frame-parent">
-            <div class="toggle">
-              <div class="track4">
-              </div>
-              <div class="knob4">
-              </div>
-            </div>
-            <div class="zvukov-vstraha">Noční režim</div>
-          </div>
-        </div>
-        <div class="menu-options">
-          <div class="menu-options-inner1">
-            <div class="menu-options-container">
-              <img class="menu-options-icon" alt="" src="Menu options.png">
-              <img class="menu-options-icon" alt="" src="Menu options.png">
-              <img class="menu-options-icon" alt="" src="Menu options.png">
-              <img class="menu-options-icon" alt="" src="Menu options.png">
-              <img class="menu-options-icon" alt="" src="Menu options.png">
-            </div>
-          </div>
-          <div class="frame-parent">
-            <div class="toggle">
-              <div class="track">
-              </div>
-              <div class="knob">
-              </div>
-            </div>
-            <div class="zvukov-vstraha">Scény</div>
-          </div>
+        <div class="card-value">50 %</div>
+        <div class="card-footer">
+          <span class="description">Aktuální vlhkost</span>
         </div>
       </div>
-      <div class="menu-options7">
-        <div class="frame-parent21">
-          <div class="menu-options-inner">
-            <div class="frame-parent22">
-              <div class="frame-parent">
-                <div class="off-wrapper">
-                  <div class="medium">Off</div>
-                </div>
-                <div class="wind">Music Sync</div>
-              </div>
-              <div class="frame-parent">
-                <div class="off-wrapper">
-                  <div class="medium">Setup</div>
-                </div>
-                <div class="wind">Schedule</div>
-              </div>
-              <div class="frame-parent">
-                <div class="off-wrapper">
-                  <div class="medium">Gorgeous</div>
-                </div>
-                <div class="wind">Scene</div>
-              </div>
-              <div class="frame-parent">
-                <div class="off-wrapper">
-                  <div class="medium">White</div>
-                </div>
-                <div class="wind">Colour</div>
-              </div>
-            </div>
-          </div>
-          <div class="frame-parent27">
-            <div class="frame-parent28">
-              <img class="fi-10206874-icon" alt="" src="Frame.svg">
+    </div>
 
-              <div class="zvukov-vstraha">27%</div>
-            </div>
-            <img class="frame-item" alt="" src="Group 13.png">
-
-            <div class="wind">Brightness</div>
-          </div>
+    <div class="column">
+      <div class="card">
+        <div class="card-header">
+          <span>Zvuková výstraha</span>
+          <label class="switch"><input type="checkbox"><span class="slider"></span></label>
         </div>
-        <div class="toggle-parent">
-          <div class="toggle">
-            <div class="track4">
-            </div>
-            <div class="knob4">
-            </div>
-          </div>
-          <div class="medium">Ambient lights</div>
+        <div class="card-value">Zapnuto</div>
+      </div>
+
+      <div class="card">
+        <div class="card-header">
+          <span>Pohybové čidlo PIR</span>
+          <label class="switch"><input type="checkbox"><span class="slider"></span></label>
+        </div>
+        <div class="card-value">Aktivní</div>
+      </div>
+
+      <div class="card">
+        <div class="card-header">
+          <span>Únik vody</span>
+          <label class="switch"><input type="checkbox"><span class="slider"></span></label>
+        </div>
+        <div class="card-value red">Zjištěn únik vody</div>
+        <div class="card-footer">
+          <span class="description">Senzor úniku</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="column">
+      <div class="card">
+        <div class="card-header">
+          <span>Bezpečný režim</span>
+          <label class="switch"><input type="checkbox"><span class="slider"></span></label>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header">
+          <span>Hlídání úniku vody</span>
+          <label class="switch"><input type="checkbox"><span class="slider"></span></label>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header">
+          <span>Noční režim</span>
+          <label class="switch"><input type="checkbox"><span class="slider"></span></label>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header">
+          <span>Scény</span>
+          <label class="switch"><input type="checkbox"><span class="slider"></span></label>
+        </div>
+        <div class="card-value">3 aktivní</div>
+      </div>
+    </div>
+
+    <div class="column">
+      <div class="card">
+        <div class="card-header">
+          <span>Ambientní světlo</span>
+          <label class="switch"><input type="checkbox"><span class="slider"></span></label>
+        </div>
+        <div class="card-header">
+          <span>Ambientní světlo</span>
+          <label class="switch"><input type="checkbox"><span class="slider"></span></label>
+        </div>
+        <div class="card-header">
+          <span>Ambientní světlo</span>
+          <label class="switch"><input type="checkbox"><span class="slider"></span></label>
+        </div>
+        <div class="card-header">
+          <span>Ambientní světlo</span>
+          <label class="switch"><input type="checkbox"><span class="slider"></span></label>
+        </div>
+        <div class="card-header">
+          <span>Ambientní světlo</span>
+          <label class="switch"><input type="checkbox"><span class="slider"></span></label>
         </div>
       </div>
     </div>
   </div>
-
-
 
 
 </body>
