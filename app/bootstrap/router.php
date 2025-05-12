9319a4b41d827ba14e2a3d2cd38784ef07cebeeb
JirikPos <Jirikpos221@gmail.com>
2025-05-12 21:13:19 +0200

    edited router sturcture

--- app/bootstrap/router.php
+++ app/bootstrap/router.php
@@ -1,28 +1,37 @@
 <?php
+
 declare(strict_types=1);
 
 $config = require_once __DIR__ . '/../config/config.php';
-require_once __DIR__ . '/../controllers/home.controller.php';
-require_once __DIR__ . '/../controllers/dashboard.controller.php';
+require_once __DIR__ . '/../views/home.controller.php';
+require_once __DIR__ . '/../views/dashboard.controller.php';
 
 $uri    = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $path   = rtrim($uri, '/');
 $method = $_SERVER['REQUEST_METHOD'];
 
-if ($method === 'GET' && ($path === '' || $path === '/')) {
-    // hlavní stránka
-    (new HomeController())->index();
-
-} elseif ($method === 'GET' && $path === '/dashboard') {
-    // dashboard stránka
-    (new DashboardController($config))->index();
-
-} elseif ($method === 'GET' && $path === '/api/data') {
-    // API endpoint
-    (new DashboardController($config))->data();
-
-} else {
-    // 404
-    http_response_code(404);
-    echo "<h1>404 – Stránka nenalezena</h1>";
+// Zpracování API route
+if ($method === 'POST' && $path === '/api/sensors') {
+  (new DashboardController($config))->data();
+  exit;
 }
+
+if ($method === 'POST' && $path === '/api/keypad') {
+  (new DashboardController($config))->data();
+  exit;
+}
+
+
+// Zpracování views route
+if ($method === 'GET' && ($path === '' || $path === '/')) {
+  (new HomeController())->index();
+  exit;
+}
+
+if ($method === 'GET' && $path === '/dashboard') {
+  (new DashboardController($config))->index();
+  exit;
+}
+
+http_response_code(404);
+echo "<h1>404 – Stránka nenalezena</h1>";
