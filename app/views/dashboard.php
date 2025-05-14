caa816df5c3f84d5b03eb6f6110235589884d28d
skrivanj <skrivan.j07@gmail.com>
2025-05-14 20:57:17 +0200

    Přepsaní cest k odkazům

--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -25,13 +25,13 @@
       </div>
       <div class="nav-bar">
         <div class="o-projektu-parent">
-          <div class="o-projektu"><a class="nav-link" href="./info-o-projektu.html">O projektu</a></div>
+          <div class="o-projektu"><a class="nav-link" href="../views/info-o-projektu.php">O projektu</a></div>
           <img class="nav-icon" alt="" src="../../public/assets/icons/about.svg">
         </div>
         <div class="nav-bar-divider">
         </div>
         <div class="ovldac-panel-parent">
-          <div class="ovldac-panel"><a class="nav-link active" href="./dashboard.html">Ovládací panel</a></div>
+          <div class="ovldac-panel"><a class="nav-link active" href="../views/dashboard.php">Ovládací panel</a></div>
           <img class="nav-icon" alt="" src="../../public/assets/icons/house.svg">
         </div>
       </div>
