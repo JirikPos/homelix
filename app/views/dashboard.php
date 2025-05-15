a9f357029f5e6372c9e08b5a35864d6af5c28f4e
skrivanj <skrivan.j07@gmail.com>
2025-05-15 15:29:48 +0200

    Opravení konflitku v dashboard

--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -24,7 +24,6 @@
       <div class="homelix-parent">
         <img class="homelix-logo" src="../../public/assets/images/logo.png" alt="">
       </div>
-<<<<<<< HEAD
       <div class="nav-bar">
         <div class="o-projektu-parent">
           <div class="o-projektu"><a class="nav-link" href="../views/info-o-projektu.php">O projektu</a></div>
@@ -35,18 +34,6 @@
         <div class="ovldac-panel-parent">
           <div class="ovldac-panel"><a class="nav-link active" href="../views/dashboard.php">Ovládací panel</a></div>
           <img class="nav-icon" alt="" src="../../public/assets/icons/house.svg">
-=======
-      <nav class="nav-bar">
-        <div class="ovldac-panel-parent">
-          <div class="ovldac-panel"><a class="nav-link active" href="./dashboard">Ovládací panel</a></div>
-          <img class="nav-icon" alt="" src="../../public/assets/icons/house.svg">
-        </div>
-        <div class="nav-bar-divider">
-        </div>
-        <div class="o-projektu-parent">
-          <div class="o-projektu"><a class="nav-link" href="./info-o-projektu">O projektu</a></div>
-          <img class="nav-icon" alt="" src="../../public/assets/icons/about.svg">
->>>>>>> develop
         </div>
 
       </nav>
